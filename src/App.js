9a07403f96cbe79913a629ffac915d6708348f3a
rabobahago <rabobahago@gmail.com>
2023-02-28 13:21:17 +0100

    Escrow.json, RealEstate.json and navigation with moralis added

--- src/App.js
+++ src/App.js
@@ -12,10 +12,29 @@ import Escrow from "./abis/Escrow.json";
 
 // Config
 import config from "./config.json";
+import { useMoralis } from "react-moralis";
 
 function App() {
+  const { enableWeb3, account, isWeb3Enabled } = useMoralis();
+  // const [account, setAccount] = useState(null);
+  // const localBlockchainData = async () => {
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  // };
+  // useEffect(() => {
+  //   try {
+  //     localBlockchainData();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
+
   return (
     <div>
+      <Navigation
+        enableWeb3={enableWeb3}
+        account={account}
+        isWeb3Enabled={isWeb3Enabled}
+      />
       <div className="cards__section">
         <h3>Welcome to Real Estate NFT</h3>
       </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,58 @@
-import logo from '../assets/logo.svg';
+import { useEffect } from "react";
+import logo from "../assets/logo.svg";
 
-const Navigation = ({ account, setAccount }) => {
+const Navigation = ({ enableWeb3, account, isWeb3Enabled }) => {
+  // const connectHandler = async () => {
+  //   const accounts = await window.ethereum.request({
+  //     method: "eth_requestAccounts",
+  //   });
+  //   setAccount(accounts[0]);
+  // };
+  useEffect(() => {
+    console.log(isWeb3Enabled);
+  }, []);
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
 
-}
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        <h1>Real Estate</h1>
+      </div>
+      {/* {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : ( */}
+
+      {account ? (
+        <button type="button" className="nav__connect">
+          connected to{" "}
+          {account.slice(0, 6) + "..." + account.slice(account.length - 4)}
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="nav__connect"
+          onClick={async () => {
+            await enableWeb3();
+          }}
+        >
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
